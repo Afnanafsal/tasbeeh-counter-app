4dad1ca3a2bb0901e27760b9fa99408cef11e1b2
AFNAN AFSAL <afnnafsal@gmail.com>
2024-03-02 08:10:43 +0530

    Update AboutUsScreen UI and add website link

--- lib/about_us_screen.dart
+++ lib/about_us_screen.dart
@@ -1,18 +1,106 @@
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class AboutUsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('About Us'),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+        title: Text(
+          'About Us',
+          style: TextStyle(
+            color: Colors.black,
+          ),
+        ),
+        leading: IconButton(
+          icon: Icon(
+            Icons.arrow_back_ios,
+            color: Colors.black,
+          ),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
+        elevation: 0,
       ),
-      body: Center(
-        child: Text(
-          'About Us Screen',
-          style: TextStyle(fontSize: 24),
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Welcome to the Tasbeeh Chain Counter!',
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Description:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'The Tasbeeh Chain Counter is a fun project developed by Afnan Afsal. It is inspired by the traditional Arabic tasbeeh chain used for counting prayers. In this app, when you touch the screen, the virtual chain moves up and starts counting. It provides a unique and interactive way to keep track of counts.',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Developer:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            Text(
+              'Afnan Afsal',
+              style: TextStyle(
+                fontSize: 16,
+              ),
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Contact:',
+              style: TextStyle(
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 10),
+            InkWell(
+              onTap: () {
+                _launchWebsite('https://afnanafsal.vercel.app');
+              },
+              child: Text(
+                'https://afnanafsal.vercel.app',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: Colors.blue,
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
+  }
+
+  void _launchWebsite(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
